251d4ba641cd42b3efedc53bbb242a67e15d6555
harshwiziq <hk23490@gmail.com>
2020-04-12 03:51:36 +0530

    15.2 Branch

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@ public class Test {
 	public static void main(String[] args) {
 		
 			System.out.println("Inside Main Method");
-			System.out.println("Master Branch");
+			System.out.println("15.2 Branch");
 	}
 }		
