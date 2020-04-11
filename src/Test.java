f3e20f6d0fb3dc2e7c6b5ad3dc017cb72c572bfb
harshwiziq <hk23490@gmail.com>
2020-04-12 03:50:59 +0530

    Updated Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 	public static void main(String[] args) {
 		
-			System.out.println("Testing webhook");
+			System.out.println("Inside Main Method");
+			System.out.println("Master Branch");
 	}
 }		
